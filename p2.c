5ba55acc1ef31c14b52479c2a73c716a598f008f
Nishit Patel <patelnishitkumarbsc24@rvu.edu.in>
2026-01-27 10:44:43 +0530

    Implement binary search in p2.c

---
+++ p2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(){
+        int a[50], n, key;
+        int low, mid, high, i;
+
+        printf("Enter number of elements:");
+        scanf("%d", &n);
+
+        printf("Enter sorted elements:");
+        for(i=0; i<n; i++){
+                scanf("%d", &a[i]);
+        }
+
+        printf("Enter element to search:");
+        scanf("%d", &key);
+
+        low = 0;
+        high =  n-1;
+
+        while (low<=high){
+                mid = (low+high)/2;
+
+                if (a[mid] == key){
+                        printf("Element found at postion %d\n", mid + 1);
+                        return 0;
+                }
+                else if(key < a[mid]){
+                        high = mid - 1;
+                }
+                else{
+                        low = mid + 1;
+                }
+        }
+        printf("Element not found.");
+        return 0;
+}
